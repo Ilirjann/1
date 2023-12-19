9dc2f02e27876d7208f93fcce251034f558097ba
Ilirjann <ilirjan.dani@outlook.com>
2023-12-19 21:02:13 +0100

    Dojo Survey

---
+++ DojoSurvey/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost("results")]
+    public IActionResult results(string Name,string Location,string Language, string Comment)
+    {
+        ViewBag.Name=Name;
+        ViewBag.Location=Location;
+        ViewBag.Language=Language;
+        ViewBag.Comment=Comment;
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
